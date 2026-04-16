51108879b1a17a41b92cfbaf71a9fdf507b02b19
Himanshu Pote <162741085+Himanshu3-dot@users.noreply.github.com>
2026-04-16 11:06:15 +0530

    Himanshutest change
    * Add footer
    
    * Add footer in Testing Branch
    
    * Fix footer HTML closing tag
    
    * Add README.md with project details
    
    * add name
    
    * himanshutest: add lorem
    
    * himanshutest: himanshu changes
    
    * himanshu:confilict
    
    * mansi: add comment
    
    * new page add
    
    * mansi: add comment
    
    * himanshu:error solve
    
    * error solve-himanshu
    
    * comments
    
    * himanshu:pr
    
    * himanshu:commit cricket
    
    ---------
    
    Co-authored-by: Mansi Krishnapurkar <127132726+Mansi24k@users.noreply.github.com>
    Co-authored-by: Mansi24k <mansikrishnapurkar@gmail.com>

--- OnlineExamModule/WebForm1.aspx.cs
+++ OnlineExamModule/WebForm1.aspx.cs
@@ -12,6 +12,10 @@ namespace OnlineExamModule//changes mitali
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            //mansi-comments add 
+            //himashu-India Women tour of South Africa: A T20I series beginning on April 17, 2026
+            //mansi- cricket is celebrated for its rich history, technical complexity, and the passionate "gentleman�s game" spirit it fosters among fans worldwide.
+
 
             //mansi
         }
